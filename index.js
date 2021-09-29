4e97fd045b866ee72765a855c8003afcb4db92b6
Heitorop <verbetskiyarthur@gmail.com>
2021-09-29 23:15:29 +0300

    Swiper&Burger

---
+++ index.js
@@ -0,0 +1,19 @@
+/** @format */
+
+new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  loop: true,
+});
+
+new Swiper(".swiper1", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  loop: true,
+});
