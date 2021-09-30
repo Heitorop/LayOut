b1909283a53b3efd22997e9ff973eda5368cecd0
Heitorop <verbetskiyarthur@gmail.com>
2021-10-01 00:07:41 +0300

    Overflow

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ new Swiper(".swiper1", {
   slidesPerView: 1,
   loop: true,
 });
+
+const checkBo = document.getElementById("menu__toggle");
+const app = document.getElementById("app");
+function check() {
+  if (checkBo.checked) {
+    app.classList.add("over");
+    console.log(app);
+  } else {
+    app.classList.remove("over");
+  }
+}
+check();
+checkBo.addEventListener("click", check);
